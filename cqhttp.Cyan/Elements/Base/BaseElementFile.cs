36aac045054d3c48ff3bd32e37b46fafda2322e2
Frank <frankli0324@hotmail.com>
2018-11-28 17:55:42 +0800

    Fix()修复

--- cqhttp.Cyan/Elements/Base/BaseElementFile.cs
+++ cqhttp.Cyan/Elements/Base/BaseElementFile.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -13,12 +14,12 @@ namespace cqhttp.Cyan.Elements.Base {
         /// could be base64://, http(s):// or file://
         /// <see>https://tools.ietf.org/html/rfc8089</see>
         /// </summary>
-        public string file { get; private set; }
+        public string fileUrl { get; private set; }
 
         /// <summary>represents whether the file has downloaded</summary>
         private bool isFixed {
             get {
-                return file.Substring (0, file.IndexOf (':')) == "base64"; //"[base64]://"
+                return fileUrl.Substring (0, fileUrl.IndexOf (':')) == "base64"; //"[base64]://"
             }
         }
         /// <summary>以二进制形式存储的文件</summary>
@@ -27,7 +28,9 @@ namespace cqhttp.Cyan.Elements.Base {
         /// <returns><see cref="NullElementException"/></returns>
         public ElementFile () : base () { }
         public ElementFile (string type, params (string key, string val) [] dict):
-            base (type, dict) { }
+            base (type, dict) { GetFilePath (); }
+        public ElementFile (string type, Dictionary<string, string> data):
+            base (type, data) { GetFilePath (); }
         /// <summary>
         /// 通过byte array构造文件Element
         /// </summary>
@@ -41,12 +44,13 @@ namespace cqhttp.Cyan.Elements.Base {
             }
         public ElementFile (string type, string url, bool useCache):
             base (type, ("file", url)) {
+                fileUrl = url;
                 if (!useCache) data["cache"] = "0";
             }
 
         private void GetFilePath () {
             try {
-                this.file = data["file"];
+                this.fileUrl = data["file"];
             } catch (KeyNotFoundException) {
                 throw new ErrorElementException ("data中没有file段***");
             }
@@ -54,20 +58,24 @@ namespace cqhttp.Cyan.Elements.Base {
         /// <summary>
         /// 下载图片并转为base64存储，并删除data中的url项
         /// 网络环境恶劣的情况下最多获取<see cref=Config.networkMaxFailure/>次
-        /// 若仍需url可从<see cref=ElementFile.file/>中获取
+        /// 若仍需url可从<see cref=ElementFile.fileUrl/>中获取
         /// </summary>
         /// <returns>返回是否成功获取到</returns>
-        private async Task<bool> Fix () {
+        public async Task<bool> Fix () {
             for (int i = 0; i < Config.networkMaxFailure; i++) {
                 try {
                     using (var http = new HttpClient ()) {
-                        bin_content = await http.GetByteArrayAsync (file);
+                        HttpResponseMessage response = await http.GetAsync (fileUrl);
+                        response.EnsureSuccessStatusCode ();
+                        bin_content = await response.Content.ReadAsByteArrayAsync ();
                         data["file"] =
                             $"base64://{Convert.ToBase64String (bin_content)}";
                     }
                     break;
                 } catch { }
             }
+            if (data.ContainsKey ("file") == false || data["file"].Length == 0)
+                return false;
             // checkFormat(bin_content)
             return true;
         }
---
+++ cqhttp.Cyan/Elements/ElementEmoji.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using cqhttp.Cyan.Elements.Base;
+namespace cqhttp.Cyan.Elements {
+    public class ElementEmoji : Element {
+        int id;
+        public ElementEmoji () : base () { }
+        public ElementEmoji (params (string, string) [] data):
+            base ("emoji", data) { }
+
+        public ElementEmoji (Dictionary<string, string> data):
+            base ("emoji", data) { }
+        public ElementEmoji (int id):
+            base ("emoji", ("id", id.ToString ())) { this.id = id; }
+
+        private void GetEmoji () {
+            try {
+                this.id = int.Parse (data["id"]);
+            } catch (Exception e) {
+                if (e is KeyNotFoundException)
+                    throw new ErrorElementException ("***data中没有id段");
+                else if (e is FormatException || e is OverflowException)
+                    throw new ErrorElementException ("***data中id项非数字或数字错误");
+            }
+        }
+    }
+}
