fa73fec2cab67dfe3584164712f97493c6bd2d9b
Frank <frankli0324@hotmail.com>
2018-12-12 18:07:48 +0800

    handle request events

---
+++ cqhttp.Cyan/Events/CQEvents/GroupAddRequestEvent.cs
@@ -0,0 +1,25 @@
+namespace cqhttp.Cyan.Events.CQEvents {
+    /// <summary>
+    /// 加个群如何?
+    /// 
+    /// 属性this.user_id中存储了请求者QQ
+    /// </summary>
+    public class GroupAddRequestEvent : Base.RequestEvent {
+        /// <summary>加入的群号</summary>
+        public long group_id { get; private set; }
+        /// <summary>请求验证信息</summary>
+        public string comment { get; private set; }
+        /// <summary>请求 flag，在调用处理请求的 API 时需要传入</summary>
+        public string flag { get; private set; }
+
+        /// <summary></summary>
+        public GroupAddRequestEvent () : base () { }
+        /// <summary></summary>
+        public GroupAddRequestEvent (long time, long user_id, long group_id, string comment, string flag):
+            base (time, user_id) {
+                this.comment = comment;
+                this.flag = flag;
+                this.group_id = group_id;
+            }
+    }
+}
