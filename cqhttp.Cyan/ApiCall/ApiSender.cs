1f2600bedec8414de2d36ed04761e430b7b181af
Frank <frankli0324@hotmail.com>
2018-12-12 17:31:56 +0800

    1.add properties to group messages(4.7.0 support) 2.Websocket Support

--- cqhttp.Cyan/ApiCall/ApiSender.cs
+++ cqhttp.Cyan/ApiCall/ApiSender.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.WebSockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using cqhttp.Cyan.ApiCall.Requests.Base;
 using Newtonsoft.Json;
@@ -14,14 +16,10 @@ namespace cqhttp.Cyan.ApiCall {
     /// </summary>
     public class HTTPApiSender {
         /// <summary></summary>
-        public async static Task<ApiResponse> DebugPostAsync (ApiRequest request) {
-            return await PostJsonAsync ("http://service.std-frank.club:233" + request.apiPath, request.content);
-        }
-        /// <summary></summary>
-        public async static Task<ApiResponse> PostJsonAsync (string dest, string param, string apiToken = "") {
+        public async static Task<ApiResponse> PostJsonAsync (string host, ApiRequest request, string apiToken = "") {
             HttpResponseMessage response = new HttpResponseMessage ();
             using (HttpContent content = new StringContent (
-                param, Encoding.UTF8, "application/json"))
+                request.content, Encoding.UTF8, "application/json"))
             using (HttpClient httpClient = new HttpClient ()) {
                 httpClient.Timeout = new System.TimeSpan (0, 0, Config.timeOut);
                 if (string.IsNullOrEmpty (apiToken) == false)
@@ -30,7 +28,7 @@ namespace cqhttp.Cyan.ApiCall {
                 try {
                     for (int i = 0; i < Config.networkMaxFailure && (i == 0 ||
                             response.IsSuccessStatusCode == false); i++) {
-                        response = await httpClient.PostAsync (dest, content);
+                        response = await httpClient.PostAsync (host + request.apiPath, content);
                     }
                 } catch (HttpRequestException) {
                     //logger.log
@@ -42,13 +40,70 @@ namespace cqhttp.Cyan.ApiCall {
                 }
             }
             try {
-                return
-                JToken.Parse (
+                request.response = JToken.Parse (
                     await response.Content.ReadAsStringAsync ()
                 ).ToObject<ApiResponse> ();
+                return request.response;
             } catch (JsonException) {
-                throw new ErrorApicallException ($"调用api{dest}时返回值无法反序列化");
+                throw new ErrorApicallException ($"调用api{request.apiPath}时返回值无法反序列化");
             }
         }
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    public class WebSocketApiSender {
+        private static Dictionary < string, (Dictionary<int, ApiResponse>, ClientWebSocket) > pool =
+            new Dictionary < string, (Dictionary<int, ApiResponse>, ClientWebSocket) > ();
+        /// <summary>
+        /// 1、若不存在与host的连接，创建之
+        /// 
+        /// 2、向host发送API请求request
+        /// </summary>
+        /// <param name="request">待发送的请求</param>
+        /// <param name="host">主机地址 (host的一个可能值为 "ws://domain:port")</param>
+        /// <param name="apiToken">access_token</param>
+        public static async Task<ApiResponse> WSSendJson (string host, ApiRequest request, string apiToken = "") {
+            string dest = host + "/api/";
+            (Dictionary<int, ApiResponse>, ClientWebSocket) current;
+            if (pool.ContainsKey (dest) == false) {
+                pool.Add (dest, (
+                    new Dictionary<int, ApiResponse> (),
+                    new ClientWebSocket ()
+                ));
+                current = pool[dest];
+                await current.Item2.ConnectAsync (new Uri (dest), new CancellationToken ());
+            } else current = pool[dest];
+            int time = DateTime.Now.Millisecond;
+            string constructor =
+                $"{{\"action\":\"{request.apiPath.Substring(1)}\","+
+                $"\"params\":{request.content},"+
+                $"\"echo\":{time}}}";
+            ApiResponse i = new ApiResponse ();
+            await current.Item2.SendAsync (
+                buffer: Encoding.UTF8.GetBytes (constructor),
+                messageType: WebSocketMessageType.Text,
+                endOfMessage: true,
+                cancellationToken: new CancellationToken ()//not going to cancel
+            );
+
+            while (current.Item1.ContainsKey (time) == false)
+                Thread.Sleep (20);
+            i = current.Item1[time];
+            current.Item1.Remove (time);
+            return i;
+        }
+        /// <summary>Websocket关闭所有连接</summary>
+        public async static void CleanUp () {
+            foreach (var i in pool) {
+                i.Value.Item1.Clear ();
+                await i.Value.Item2.CloseAsync (
+                    closeStatus: WebSocketCloseStatus.NormalClosure,
+                    statusDescription: "client shutdown",
+                    cancellationToken : new CancellationToken ()
+                );
+            }
+        }
+
     }
 }
--- cqhttp.Cyan/Program.cs
+++ cqhttp.Cyan/Program.cs
@@ -8,42 +8,7 @@ using Newtonsoft.Json.Linq;
 
 namespace cqhttp.Cyan {
     class Program {
-        /*
-{
-    "font":31108208,
-    "message":"asdf",
-    "message_id":1002,
-    "message_type":"private",
-    "post_type":"message",
-    "raw_message":"asdf",
-    "self_id":2956005355,
-    "sender":{
-        "age":48,
-        "nickname":"Frank\xe2\x84\xa2",
-        "sex":"male",
-        "user_id":745679136
-    },
-    "sub_type":"friend",
-    "time":1543816446,
-    "user_id":745679136
-}
-        */
-        class A { }
-        class B : A { }
-        class C : B { }
-        private static Regex cqCodeMatch
-            = new Regex (@"\[CQ:([\w\-\.]+?)(?:,([\w\-\.]+?)=(.+?))*\]");
         static void Main (string[] args) {
-            // Match i = cqCodeMatch.Match("asdf[CQ:image,file=DE69C8D4C54997FC5ECBE475153651BE.jpg,url=https://c2cpicdw.qpic.cn/offpic_new/745679136//73da0548-ac93-4d5e-abd8-71138f019b28/0?vuin=2956005355&amp;term=2]");
-            // Console.WriteLine(i.NextMatch().Success);
-            // foreach (var j in i.Groups){
-            //     Console.WriteLine(j.ToString());
-            // }
-            //JArray jArray=JArray.Parse("{\"type\":\"text\",\"data\":{\"text\":\"asdf\"}}");
-            //Console.WriteLine(jArray[0]);
-            // Console.WriteLine (Convert.ToChar('a'+1));
-            // string b = new Dictionary<string, string> () ["asdf"];
-            // Console.WriteLine ("Hello World!");
             // Tests.ElementBuild.Test();
             // Tests.MessageBuild.Test();
 
