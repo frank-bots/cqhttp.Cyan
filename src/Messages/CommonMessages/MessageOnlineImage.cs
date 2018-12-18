8de08ff4f2018c348202d8183abcb68cf4bee64d
Frank <frankli0324@hotmail.com>
2018-12-18 14:56:46 +0800

    add CommonMessage

---
+++ src/Messages/CommonMessages/MessageOnlineImage.cs
@@ -0,0 +1,14 @@
+using cqhttp.Cyan.Messages;
+using cqhttp.Cyan.Messages.CQElements;
+
+namespace src.Messages.CommonMessages {
+    /// <summary>
+    /// 在线图片
+    /// </summary>
+    public class MessageOnlineImage : Message {
+        /// <param name="url">图片的url</param>
+        /// <param name="useCache">是否缓存</param>
+        public MessageOnlineImage (string url, bool useCache = true):
+            base (new ElementImage (url, useCache)) { }
+    }
+}
