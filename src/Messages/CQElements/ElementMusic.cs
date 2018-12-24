4daa306b0f5fd39667e52a5eeeb6c8d15753fe62
Frank <Frank@FrankdeMacBook-Pro.local>
2018-12-25 01:11:32 +0800

    implemented netease music id api call

--- src/Messages/CQElements/ElementMusic.cs
+++ src/Messages/CQElements/ElementMusic.cs
@@ -11,17 +11,26 @@ namespace cqhttp.Cyan.Messages.CQElements {
         ///
         public ElementMusic (string type, string keyword) : base ("music", ("type", type), ("id", GetMusicID (type, keyword).Result)) { }
         private async static Task<string> GetMusicID (string type, string keyword) {
-
-            using (var i = new HttpClient ()) {
-                var res = JToken.Parse (await i.GetStringAsync (new System.Uri ("https://music.163.com/api/search/get?type=1&limit=1&s=" + keyword)));
-                if (
-                    res["code"].ToObject<int> () != 200 ||
-                    res["result"]["songCount"].ToObject<int> () == 0
-                ) {
-                    return "511728615"; // 404 Not Found(Prod.by CashMoneyAP)
+            if (type == "163")
+                using (var i = new HttpClient ()) {
+                    var res = JToken.Parse (await i.GetStringAsync (new System.Uri ("https://music.163.com/api/search/get?type=1&limit=1&s=" + keyword)));
+                    if (
+                        res["code"].ToObject<int> () != 200 ||
+                        res["result"]["songCount"].ToObject<int> () == 0
+                    ) {
+                        return "511728615"; // 404 Not Found(Prod.by CashMoneyAP)
+                    }
+                    Logger.Log (Enums.Verbosity.INFO, $"解析了163音乐搜索{keyword}的搜索结果");
+                    return res["result"]["songs"][0]["id"].ToString ();
                 }
-                return res["result"]["songs"][0]["id"].ToString ();
+            // else if (type == "qq")
+
+            // else if (type == "xiami")
+            else if (type == "qq" || type == "xiami") {
+                Logger.Log (Enums.Verbosity.ERROR, "暂未实现163与xiami搜索结果的解析");
+                throw new System.NotImplementedException ("由于作者太菜,还没掌握qq和虾米音乐的搜索技巧，所以抱歉,这里抛出了一个微小的异常");
             }
+            throw new Exceptions.ErrorElementException ("请将type设为163,qq,xiami之一");
         }
     }
 }
