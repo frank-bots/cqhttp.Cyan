d95450615179ac2c27a4a0e1292a32d07b0b56fd
Frank <frankli0324@hotmail.com>
2019-01-04 20:53:30 +0800

    考前最后一发commit

--- src/Utils.cs
+++ src/Utils.cs
@@ -56,6 +56,23 @@ namespace cqhttp.Cyan.Utils {
         }
     }
     /// <summary>
+    /// 一些有用的构造
+    /// </summary>
+    public class Get {
+        /// <summary>
+        /// 群头像(url的形式)
+        /// </summary>
+        public string GroupAvatarUrl (long group_id) =>
+            $"https://p.qlogo.cn/gh/{group_id}/{group_id}/0";
+
+        /// <summary>
+        /// QQ用户头像(url的形式)
+        /// </summary>
+        public string UserAvatarUrl (long user_id) =>
+            $"https://q1.qlogo.cn/g?b=qq&nk={user_id}&s=0";
+
+    }
+    /// <summary>
     /// 记录发送的消息
     /// </summary>
     public class MessageTable {
