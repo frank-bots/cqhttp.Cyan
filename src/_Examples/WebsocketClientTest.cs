2c156dcf38f661549484469cad4d0f696e8bda7e
Frank <frankli0324@hotmail.com>
2019-01-31 14:30:51 +0800

    format code

--- src/_Examples/WebsocketClientTest.cs
+++ src/_Examples/WebsocketClientTest.cs
@@ -1,16 +1,12 @@
-﻿using cqhttp.Cyan.Events.CQEvents;
+﻿using System.Collections.Generic;
+using cqhttp.Cyan.Events.CQEvents;
 using cqhttp.Cyan.Events.CQResponses;
 using cqhttp.Cyan.Instance;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace cqhttp.Cyan.Examples
 {
-    class WebsocketClientTest
-    {
-        public static void Test()
-        {
+    class WebsocketClientTest {
+        public static void Test () {
             //设置日志
             Logger.LogType = Enums.LogType.Console | Enums.LogType.File;
             Logger.LogLevel = Enums.Verbosity.ALL;
@@ -18,23 +14,21 @@ namespace cqhttp.Cyan.Examples
             string accessUrl = "ws://127.0.0.1:6700";
             string token = "";
             string eventUrl = "ws://127.0.0.1:6700/event/";
-            var cli = new CQWebsocketClient(accessUrl, token, eventUrl);
+            var cli = new CQWebsocketClient (accessUrl, token, eventUrl);
 
-            var users = new List<long>() { 1234567890 };
-            cli.OnEventAsync += async (client, e) =>
-            {
+            var users = new List<long> () { 1234567890 };
+            cli.OnEventAsync += async (client, e) => {
                 // 复读指定用户私聊消息
-                if (e is PrivateMessageEvent me && users.Contains(me.sender_id))
-                {
-                    await client.SendMessageAsync(
+                if (e is PrivateMessageEvent me && users.Contains (me.sender_id)) {
+                    await client.SendMessageAsync (
                         Enums.MessageType.private_,
                         me.sender_id,
                         me.message
                     );
                 }
-                return new EmptyResponse();
+                return new EmptyResponse ();
             };
-            System.Console.ReadLine();// 务必在所有bot中加上这一句
+            System.Console.ReadLine (); // 务必在所有bot中加上这一句
         }
     }
 }
