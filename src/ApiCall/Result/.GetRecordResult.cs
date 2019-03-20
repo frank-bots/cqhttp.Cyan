64634f1b4f2efae9fa52fe9ff99d04c92ab9afa5
Frank <frankli0324@hotmail.com>
2019-03-20 17:52:38 +0800

    Add GetRecord API call

---
+++ src/ApiCall/Result/.GetRecordResult.cs
@@ -0,0 +1,21 @@
+using Newtonsoft.Json.Linq;
+
+namespace cqhttp.Cyan.ApiCall.Results {
+    ///
+    public class GetRecordResult : Base.ApiResult {
+        /// <summary>
+        /// 获取到的文件名或绝对路径
+        /// </summary>
+        public string file;
+        ///
+        public override void Parse (string result) {
+            var i = this.PreCheck(result);
+            try {
+                file = i.raw_data["file"].ToString();
+            } catch {
+                Logger.Log (Enums.Verbosity.ERROR, "调用发送消息API未返回file");
+                throw new Exceptions.ErrorApicallException ();
+            }
+        }
+    }
+}
