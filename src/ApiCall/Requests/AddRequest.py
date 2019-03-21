9cda3b95f03a09e6c9c4ebb3eab16f3ebc11f66b
Frank <frankli0324@hotmail.com>
2019-03-21 09:25:33 +0800

    judge string in script

--- src/ApiCall/Requests/AddRequest.py
+++ src/ApiCall/Requests/AddRequest.py
@@ -1,9 +1,10 @@
-api_name = 'get_image'
+api_name = 'clean_data_dir'
 class_name = ''.join([i.capitalize() for i in api_name.split('_')])+'Request'
 api_path = '/'+api_name
 base_class = 'ApiRequest'
-params = ['string filename']
-summary = '获取图片路径'
+params = [ 'string data_dir' ]
+summary = '用于清空插件的日志文件。'
+is_empty = True
 
 template = f'''
 using cqhttp.Cyan.ApiCall.Requests.Base;
@@ -16,18 +17,23 @@ namespace cqhttp.Cyan.ApiCall.Requests {{
         {';'.join(params)};
         ///
         public {class_name} ({','.join(params)}) : base ("{api_path}") {{
-            this.response = new Results.{class_name[:-7]}Result();
+            this.response = new Results.{'Empty' if is_empty else f'{class_name[:-7]}'}Result();
             {';'.join(['this.{0} = {0}'.format(i.split(' ')[1]) for i in params])};
         }}
         ///
         public override string content {{
             get {{
-                return $"{{{{{','.join(['{1}"{0}{1}"={{{0}}}'.format(i.split(' ')[1],'~')for i in params])}}}}}";
+                return $"{{{{'''+ \
+                    ','.join(['~"'+i[1]+'~":'+('{'+i[1]+'}' if i[0]!='string' else f'{{Config.asJsonStringVariable({i[1]})}}') for i in [i.split(' ') for i in params]])+ \
+f'''}}}}";
             }}
         }}
     }}
 }}
-'''.replace('~', '\\')
+'''
+template = template.replace('~','\\')
+
+print(template)
 x = open(class_name+'.cs', 'w')
 x.write(template)
 x.close()
