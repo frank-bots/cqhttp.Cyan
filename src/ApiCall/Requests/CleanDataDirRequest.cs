143b97ee37575751a9c6d4f391582b201659b5f7
Frank <frankli0324@hotmail.com>
2019-03-21 09:25:10 +0800

    add cleaning apis

---
+++ src/ApiCall/Requests/CleanDataDirRequest.cs
@@ -0,0 +1,21 @@
+using cqhttp.Cyan.ApiCall.Requests.Base;
+
+namespace cqhttp.Cyan.ApiCall.Requests {
+    /// <summary>
+    /// 用于清空插件的日志文件。
+    /// </summary>
+    public class CleanDataDirRequest : ApiRequest {
+        string data_dir;
+        ///
+        public CleanDataDirRequest (string data_dir) : base ("/clean_data_dir") {
+            this.response = new Results.EmptyResult ();
+            this.data_dir = data_dir;
+        }
+        ///
+        public override string content {
+            get {
+                return $"{{\"data_dir\":{Config.asJsonStringVariable(data_dir)}}}";
+            }
+        }
+    }
+}
---
+++ src/ApiCall/Requests/CleanPluginLogRequest.cs
@@ -0,0 +1,19 @@
+using cqhttp.Cyan.ApiCall.Requests.Base;
+
+namespace cqhttp.Cyan.ApiCall.Requests {
+    /// <summary>
+    /// 用于清空插件的日志文件。
+    /// </summary>
+    public class CleanPluginLogRequest : ApiRequest {
+        ///
+        public CleanPluginLogRequest () : base ("/clean_plugin_log") {
+            this.response = new Results.EmptyResult ();
+        }
+        ///
+        public override string content {
+            get {
+                return $"{{}}";
+            }
+        }
+    }
+}
