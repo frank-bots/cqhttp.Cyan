90c2cc2cbb5828a765014e01610f6c80e650791b
Frank <frankli0324@hotmail.com>
2020-01-27 23:31:07 +0800

    graceful shutdown for websocket listener

--- cqhttp.Cyan/Clients/Listener/WebsocketListener.cs
+++ cqhttp.Cyan/Clients/Listener/WebsocketListener.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
 using cqhttp.Cyan.Events.CQResponses;
 using cqhttp.Cyan.Events.CQResponses.Base;
 
@@ -8,6 +10,7 @@ namespace cqhttp.Cyan.Clients.Listeners {
     class WebsocketListener : Listener {
         ClientWebSocket client;
         public Callers.ICaller caller;
+        CancellationTokenSource ctoken_source;
         public WebsocketListener (string event_url, string access_token) {
             if (event_url.EndsWith ("/event")) {
                 event_url += '/';
@@ -17,28 +20,30 @@ namespace cqhttp.Cyan.Clients.Listeners {
             event_url += !string.IsNullOrEmpty (access_token) ? "?access_token=" + access_token : "";
 
             client = new ClientWebSocket ();
-            client.ConnectAsync (
-                new System.Uri (event_url), new System.Threading.CancellationToken ()
-            );
-            System.Threading.Tasks.Task.Run (async () => {
+            ctoken_source = new CancellationTokenSource ();
+            Task.Run (async () => {
+                await client.ConnectAsync (
+                    new System.Uri (event_url),
+                    ctoken_source.Token
+                );
                 byte[] recv_buffer = new byte[2048];
                 List<byte> message = new List<byte> ();
-                while (true) {
-                    switch (client.State) {
-                    case WebSocketState.Open:
-                        var t = await client.ReceiveAsync (
-                            recv_buffer,
-                            new System.Threading.CancellationToken ()
-                        );
-                        if (!t.EndOfMessage) {
-                            message.AddRange (recv_buffer);
-                        } else {
-                            message.AddRange (recv_buffer.ToList ().GetRange (0, t.Count));
-                            Process (System.Text.Encoding.UTF8.GetString (message.ToArray ()));
-                            message.Clear ();
-                        }
-                        break;
+                while (
+                    client.State != WebSocketState.Open &&
+                    ctoken_source.Token.IsCancellationRequested == false
+                ) {
+                    var t = await client.ReceiveAsync (
+                        recv_buffer,
+                        ctoken_source.Token
+                    );
+                    if (!t.EndOfMessage) {
+                        message.AddRange (recv_buffer);
+                    } else {
+                        message.AddRange (recv_buffer.ToList ().GetRange (0, t.Count));
+                        Process (System.Text.Encoding.UTF8.GetString (message.ToArray ()));
+                        message.Clear ();
                     }
+                    break;
                 }
             });
         }
@@ -63,6 +68,13 @@ namespace cqhttp.Cyan.Clients.Listeners {
                     $"处理事件时发生未处理的异常{e},错误信息为{e.Message}"
                 );
             }
+        }
+        ~WebsocketListener () {
+            client.CloseAsync (
+                WebSocketCloseStatus.NormalClosure, "",
+                new CancellationToken ()
+            ).RunSynchronously ();
+            ctoken_source.Cancel ();
         }
     }
 }
