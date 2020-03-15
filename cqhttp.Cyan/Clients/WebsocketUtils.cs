6f458d59878809195a146bf3dd197dc127dbf5ba
Frank <frankli0324@hotmail.com>
2020-03-15 19:35:53 +0800

    fixed serious bug bump version && remove redundant code

--- cqhttp.Cyan/Clients/WebsocketUtils.cs
+++ cqhttp.Cyan/Clients/WebsocketUtils.cs
@@ -1,7 +1,4 @@
 using System.Collections.Generic;
-using System.Net.WebSockets;
-using System.Threading;
-using System.Threading.Tasks;
 using cqhttp.Cyan.Utils;
 using Fleck;
 
@@ -15,18 +12,10 @@ namespace cqhttp.Cyan.Clients.WebsocketUtils {
         static WebsocketDaemon () {
             Fleck.FleckLog.LogAction = (level, m, e) => {
                 switch (level) {
-                case LogLevel.Info:
-                    Log.Info (m);
-                    break;
-                case LogLevel.Warn:
-                    Log.Warn (m);
-                    break;
-                case LogLevel.Error:
-                    Log.Error (m);
-                    break;
-                case LogLevel.Debug:
-                    Log.Debug (m);
-                    break;
+                case LogLevel.Info: Log.Info (m); break;
+                case LogLevel.Warn: Log.Warn (m); break;
+                case LogLevel.Error: Log.Error (m); break;
+                case LogLevel.Debug: Log.Debug (m); break;
                 }
             }; // unify logs
         }
@@ -74,69 +63,5 @@ namespace cqhttp.Cyan.Clients.WebsocketUtils {
                 }
             }
         }
-    }
-    /// <summary>
-    /// As Websocket Client
-    /// </summary>
-    static class ConnectionPool {
-        private static Dictionary<string, ClientWebSocket> pool =
-            new Dictionary<string, ClientWebSocket> ();
-        static SemaphoreSlim lock_ = new SemaphoreSlim (1, 1);
-        private static async Task Connect (string uri) {
-            pool[uri] = new ClientWebSocket ();
-            await pool[uri].ConnectAsync (
-                new System.Uri (uri),
-                new CancellationToken ()
-            );
-        }
-
-        private static async Task EnsureConnected (string uri) {
-            if (pool.ContainsKey (uri)) {
-                await new System.Func<bool> (
-                    () => pool[uri].State != WebSocketState.Connecting
-                ).TimeOut ("websocket 连接超时");
-                if (pool[uri].State == WebSocketState.Open)
-                    return;
-                pool[uri].Abort ();
-                pool[uri].Dispose ();
-            }
-            await Connect (uri);
-        }
-        /// <summary>
-        /// 不要调用
-        /// </summary>
-        public static async Task<string> RequestAsync (string uri, string obj) {
-            await lock_.WaitAsync ();
-            try {
-                string result = "";
-                await EnsureConnected (uri);
-                await pool[uri].SendAsync (
-                    buffer: System.Text.Encoding.UTF8.GetBytes (obj),
-                    messageType: WebSocketMessageType.Text,
-                    endOfMessage: true,
-                    cancellationToken: new CancellationToken () //not going to cancel
-                );
-                byte[] buffer = new byte[1024 * 3];
-                while (true) {
-                    var res = await pool[uri].ReceiveAsync (
-                        buffer: buffer,
-                        cancellationToken: new CancellationToken ()
-                    );
-                    result += System.Text.Encoding.UTF8.GetString (buffer).TrimEnd ('\0');
-                    if (res.EndOfMessage)
-                        break;
-                }
-                return result;
-            } catch (System.Exception e) {
-                Log.Error ("Websocket调用API失败");
-                Log.Debug ("调用:" + obj.ToString ());
-                Log.Debug ("Traceback:" + e.StackTrace);
-                throw new Exceptions.NetworkFailureException ("调用API失败");
-            } finally {
-                lock_.Release ();
-            }
-        }
-        public static void DisposeTarget (string uri) =>
-            pool[uri].Dispose ();
     }
 }
